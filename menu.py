3842fd1a2e9c652314bd7957db3939f022f4618c
katzrkool <katzrkool@protonmail.com>
2017-09-28 13:29:42 -0500

    Added Comments

--- menu.py
+++ menu.py
@@ -7,6 +7,7 @@ import os
 
 doIExist = True
 
+#tries to open prefrences and set schoolName and menuSelection variables
 try:
     with open("{}/preferences.json".format(os.path.dirname(__file__)),"r") as f:
         preferences = json.load(f)
@@ -15,6 +16,7 @@ try:
             menuSelection = preferences["menu"]
         except:
             pass
+#if not it asks for the school name and verifies that it exists
 except:
     doIExist = False
     schoolName = input("What's your myschooldining code? Found after the url on their site. \nExample: For myschooldining.com/thenewschool, thenewschool would be your code\n")
@@ -26,16 +28,21 @@ except:
         except:
             break
 
+#puts the menu in soup thing
 soup = BeautifulSoup(requests.get("http://myschooldining.com/{}".format(schoolName)).text, "html.parser")
 def pickMenuSelector():
     global menuSelection
+    #finds the button div
     menuCenter = soup.findAll("span", attrs={"class":"section-title"})
     menuCenter = menuCenter[1]
+    #finds the buttons
     button = menuCenter.findAll("button", attrs={"class":"change-school"})
+    #if 1 button, use that button
     if len(button) == 1:
         date = determineWhichMenuToGrab()
         litButton = button[0].get("id")
         formatAndPrintDatData(litButton, date)
+    #if more button, ask user which button to use
     else:
         if doIExist == False:
             for i in range(0,len(button)):
@@ -62,18 +69,25 @@ def determineWhichMenuToGrab():
     return datezers
 
 def formatAndPrintDatData(litbutton,date):
+    #what day is it?
     day = str(date.day)
+    #sets up classes
     day = "day-" + day
     menuClass = "menu-"+litbutton
+    #finds the menu location
     menu = soup.find("div", class_="{} {} menu-location".format(day, menuClass))
+    #finds all menu items
     menu = menu.findAll("span", class_="no-print")
+    #if its a category, get rid of it.
     menu = [i for i in menu if not i["class"] == ["month-category","no-print"]]
+    #no more whitespace and print everything
     for i in range(0,len(menu)):
         menu[i] = menu[i].text
         menu[i] = menu[i].replace("\xa0", "")
         menu[i] = menu[i].replace("\n","")
         menu[i] = menu[i].replace("                             ","")
         print(menu[i])
+    #ask to save info if info doesn't exist
     if doIExist == False:
         choice = "Netscape Navigator"
         while choice != "y" and choice != "n":
@@ -81,6 +95,7 @@ def formatAndPrintDatData(litbutton,date):
         if choice == "y":
             saveDatData()
 
+#save data
 def saveDatData():
     with open("{}/preferences.json".format(os.path.dirname(__file__)),"w") as fp:
         try:
